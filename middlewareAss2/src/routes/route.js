c9afe92f672082c366724f1fe457e976f80a2189
unknown <shravani170@gmail.com>
2021-11-23 22:20:04 +0530

    middleware assignment 2

---
+++ middlewareAss2/src/routes/route.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/userController')
+const productController = require('../controllers/productController')
+const orderController = require('../controllers/orderController')
+const appMiddleware = require('../middlewares/appMiddleware')
+
+router.post('/users', appMiddleware.validateAppType,userController.createUser);
+//For JWT session
+//router.get('/users/:userId', userController.getDetails)
+//For JWT session
+//router.post('/login', userController.login)
+router.post('/products', productController.createProduct);
+router.post('/orders', appMiddleware.validateAppType, orderController.createOrder);
+
+
+module.exports = router;
